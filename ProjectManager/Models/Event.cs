c96927b7410c838d40eb014055c4f6ce6428939d
Hannah <hrose@caltech.edu>
2022-08-14 19:58:48 -0500

    Added Databases

---
+++ ProjectManager/Models/Event.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace ProjectManager.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+        
+        [ForeignKey("EventTypes")]
+        public int EventTypeId { get; set; }
+        public EventType EventType { get; set; }
+
+        [ForeignKey("NTasks")]
+        public int TaskId { get; set; }
+        public NTask Task { get; set; }
+
+        [ForeignKey("AspNetUsers")]
+        public string UserId { get; set; } = String.Empty;
+        public IdentityUser User { get; set; }
+
+        [Required]
+        public int Value { get; set; } = 1;
+
+        [Required]
+        public DateTime TimeEntered { get; set; } = default;
+    }
+}
---
+++ ProjectManager/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManager.Models;
+
+public class Project
+{
+    [Key]
+    public int ProjectId { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
+
